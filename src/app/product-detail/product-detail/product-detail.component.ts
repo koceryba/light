70eba90ab4191853b5cb78baf9439301c24a1ad5
sergey <you@example.com>
2019-11-06 02:40:01 +0200

    first commit

--- src/app/product-detail/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail/product-detail.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductDetailService } from './product-detail.service';
-import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
